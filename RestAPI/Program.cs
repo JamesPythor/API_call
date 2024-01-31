87ae872aa9929f6418e82f33296820cf46a83e49
Santiago L <119707073+JamesPythor@users.noreply.github.com>
2024-01-31 17:19:41 -0600

    Add files via upload

---
+++ RestAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+	Method = HttpMethod.Get,
+	RequestUri = new Uri("https://pokemon-api3.p.rapidapi.com/pokemon?name=salamence"),
+	Headers =
+	{
+		{ "X-RapidAPI-Key", "b3733c7c03mshc70ab225b166faep18b027jsne23871af4ff5" },
+		{ "X-RapidAPI-Host", "pokemon-api3.p.rapidapi.com" },
+	},
+};
+using (var response = await client.SendAsync(request))
+{
+	response.EnsureSuccessStatusCode();
+	var body = await response.Content.ReadAsStringAsync();
+	Console.WriteLine(body);
+}
+
+Console.WriteLine("API executed successfully");
